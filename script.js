9b98ba529d5336f8217a971bd02346e38bd18aff
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-11-12 13:31:52 +0300

    Design and Ddark/light mode enabling

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    let darkModeButton = document.querySelector(".dark_mode_btn");
+    let body = document.querySelector("body");
+    let display = document.querySelector(".display");
+    let result = document.querySelector('#result');
+    let normalButton = document.querySelectorAll("#Normal_btn");
+    let deleteSingleNum  = document.querySelector("#delete_single_num");
+    let equalTo = document.querySelector("#equalTo");
+    let history = document.querySelector("#history");
+    let clear = document.querySelector("#clear");
+    /* Dark mode*/
+    let darkModeStatus = false;
+    darkModeButton.addEventListener('click', function(event){
+        body.classList.toggle('dark_mode_active');
+        if (darkModeStatus == false) {
+            this.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+            darkModeStatus = true;
+        }else{
+            this.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+            darkModeStatus = false;
+        }
+    });
+
+
+});
